f8660336f33f4cf8e56cf9ceaac9258ca09bd92a
Olg <o.olpinski@gmail.com>
2018-08-17 18:22:02 +0200

    Comments to Interface

--- src/Persons/Attackable.java
+++ src/Persons/Attackable.java
@@ -2,9 +2,23 @@ package Persons;
 
 public interface Attackable
 {
+    //public static final int A = 5;
+
     void getHurt(int amount, int armorPenetration);
     void attack(Person enemy);
-    int damageValue(int amount, int armorPenetration);
-    int calcArmorPenetration(int armorPenetration);
+    int damageValue(int amount, int armorPenetration); // to powinno wypaść z interfejsu
+    int calcArmorPenetration(int armorPenetration);    // to też
     void die ();
+    static void died ()
+    {
+        //ok statyczne mogą mie ciało
+    }
+
+    //default może mieć ciało, będzie standardowo implementowaną metodą, możemy użyć @Override
+    default String diedd ()
+    {
+        return "ABC";
+    }
+
+    //metody domyślne mogą mieć ciało
 }
